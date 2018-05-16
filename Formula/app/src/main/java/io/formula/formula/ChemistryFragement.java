feb8b6acd81b4b0abafdbbd64999a7d89a8c5bc8
aibek <sarievaibek@gmail.com>
2018-05-15 23:42:34 -0500

    updated trigonometry

--- Formula/app/src/main/java/io/formula/formula/ChemistryFragement.java
+++ Formula/app/src/main/java/io/formula/formula/ChemistryFragement.java
@@ -23,7 +23,7 @@ public class ChemistryFragement extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chemistry_fragement, container, false);
+        return inflater.inflate(R.layout.fragment_trigonometry, container, false);
     }
 
 }
