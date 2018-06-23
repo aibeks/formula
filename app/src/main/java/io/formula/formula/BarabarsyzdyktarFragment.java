06f4bb65a2fc800dd139b56bde23fccbc27e5512
final commit <>
2018-06-23 16:10:48 -0500

    initial commit

---
+++ app/src/main/java/io/formula/formula/BarabarsyzdyktarFragment.java
@@ -0,0 +1,24 @@
+package io.formula.formula;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by aibeksariev on 5/31/18.
+ */
+
+public class BarabarsyzdyktarFragment extends Fragment {
+
+    public BarabarsyzdyktarFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_barabarsyzdyktar, container, false);
+    }
+}
---
+++ app/src/main/java/io/formula/formula/TestFragment.java
@@ -0,0 +1,25 @@
+package io.formula.formula;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by aibeksariev on 5/31/18.
+ */
+
+public class TestFragment extends Fragment {
+
+    public TestFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_test, container, false);
+    }
+}
